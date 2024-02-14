e5dd91f9c19a434eb625f2e1930d53dfb53dfa4c
Makanda-h <hopemwaura39@gmail.com>
2024-02-14 08:49:46 -0800

    updates

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-const callMeMoby = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
-}
-module.exports = {
-  callMeMoby: callMeMoby
-}
+};
