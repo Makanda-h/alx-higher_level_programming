29c5eda39fd8121c9d0d0d74a9ea7cf8197d6585
Makanda-h <hopemwaura39@gmail.com>
2024-02-18 09:49:50 -0800

    updated

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-'''
-prints first State object from the database hbtn_0e_6_usa
-'''
-from sys import argv
+""" prints the first State object from the database hbtn_0e_6_usa
+"""
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1],
-        argv[2],
-        argv[3]))
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    res = session.query(State).first()
-    if res is None:
-        print('Nothing')
+    instance = session.query(State).first()
+    if instance is None:
+        print("Nothing")
     else:
-        print('{}: {}'.format(res.id, res.name))
-    session.close()
+        print(instance.id, instance.name, sep=": ")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-'''
-contains class definition of a city and an instance
-Base = declarative_base()
-'''
-
-from sys import argv
+"""
+Contains the class definition of a City
+"""
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
 
 class City(Base):
-    '''
-    maps to table cities
-    '''
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
